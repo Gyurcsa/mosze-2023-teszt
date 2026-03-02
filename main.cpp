7f15c41a7777a9069f9f3848ef5297085c631762
Gyurcsa <gyuriczagergo776@gmail.com>
2026-03-02 23:52:16 +0100

    Javított változat

--- main.cpp
+++ main.cpp
@@ -4,21 +4,21 @@ constexpr int N_ELEMENTS = 100;
 
 int main()
 {
-    int *b = new int[NELEMENTS]; //Rossz változónév - "_" hiányzik
-    std::cout << '1-100 ertekek duplazasa' //";" karakter hiányzik a végén
-    for (int i = 0;) //Feltétel hiányzik
+    int *b = new int[N_ELEMENTS]; //Rossz változónév - "_" hiányzik
+    std::cout << '1-100 ertekek duplazasa'; //";" karakter hiányzik a végén
+    for (int i = 0; i < N_ELEMENTS; i++) //Feltétel hiányzik
     {
         b[i] = i * 2;
     }
-    for (int i = 0; i; i++)
+    for (int i = 0; i < N_ELEMENTS; i++) //Feltétel hiányzik
     {
-        std::cout << "Ertek:" //Vége hiányzik
+        std::cout << "Ertek:" << b[i] << std::endl;
     }    
     std::cout << "Atlag szamitasa: " << std::endl;
-    int atlag;
-    for (int i = 0; i < N_ELEMENTS, i++) // "," helyett ";" kell
+    int atlag=0; // átlag változó hiányos
+    for (int i = 0; i < N_ELEMENTS; i++) // "," helyett ";" kell
     {
-        atlag += b[i] // ";" karakter hiányzik a végén
+        atlag += b[i]; // ";" karakter hiányzik a végén
     }
     atlag /= N_ELEMENTS;
     std::cout << "Atlag: " << atlag << std::endl;
