3403e1c84bfc77e3e56822acac1449d8ab9ad809
0wl <patakiarpad11@gmail.com>
2026-03-02 23:59:37 +0100

    Uzenet Hozzaadva

--- main.cpp
+++ main.cpp
@@ -22,6 +22,6 @@ int main()
     }
     atlag /= N_ELEMENTS;
     std::cout << "Atlag: " << atlag << std::endl;
-    std::count << "Minden Prima" << std::endl;
+    std::cout << "Minden Prima" << std::endl;
     return 0;
 }
